0eedb0e7a32b6bd66da898dcc0b49a72a561fd50
Utarasetyaw <tarazekage@gmail.com>
2025-08-21 18:10:35 +0700

    fix(main): update server port from 3007 to 3006

--- src/main.ts
+++ src/main.ts
@@ -64,6 +64,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3007);
+  await app.listen(3006);
 }
 bootstrap();
